a7e216e94bd8d8cdcc838521aa0325185e3f9d94
ErikTheBlacksmith <eam53@njit.edu>
2024-06-20 16:59:05 -0400

    renamed clusters

--- wrapper.R
+++ wrapper.R
@@ -20,6 +20,8 @@ sc.cells <- cluster(seuratObj = NULL, seuratFile = "./saveddata/preprocessed_cel
 atlas.rename <- c("GC_Estrous"            = "GC_Luteinizing and CL",
                   "GC_CL_Lytic"           = "GC_Luteinizing and CL",
                   "GC_CL_Active"          = "GC_Luteinizing and CL",
+                  "GC_Antral"             = "GC_Mural",
+                  "GC_Preantral"          = "GC_Cumulus",
                   "M_Immature Theca"      = "M_Early Theca",
                   "M_Dividing Mesenchyme" = "M_Early Theca", 
                   "M_Cortical Stroma"     = "M_Steroidogenic Stroma",
@@ -28,7 +30,7 @@ atlas.rename <- c("GC_Estrous"            = "GC_Luteinizing and CL",
 sc.cells <- mapcells(seuratObj = NULL, seuratFile = "./saveddata/clustered_cells.rds",
                      atlasFile = "./morrisdata/ovary_0.rds",
                      make.plots = TRUE, plotLocation = "./images/",
-                     atlas.rename = atlas.rename, PC = 30, umapPC = 20,
+                     atlas.rename = atlas.rename, PC = 20, umapPC = 20,
                      anchor.n.trees = 50, anchor.k.anchor = 5, anchor.k.score = 30,
                      saverds = TRUE, write.rdsfile = "./saveddata/mapped_cells.rds",
                      marker.folder = "./morrismarkers/",
@@ -37,8 +39,8 @@ sc.cells <- mapcells(seuratObj = NULL, seuratFile = "./saveddata/clustered_cells
 DE.data <- calcDE(sc.cells,
                   savedata = TRUE, DEFolder = "./data/")
 
-enrichvals = list(c(1, "GC-Antral", .25),
-                  c(1, "GC-Antral", .05),
+enrichvals = list(c(1, "GC-Mural", .25),
+                  c(1, "GC-Mural", .05),
                   c(0, "Granulosa", .05),
                   c(0, "Granulosa", .25))
 for (eval in enrichvals) {
